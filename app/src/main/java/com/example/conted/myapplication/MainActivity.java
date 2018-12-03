2c69aa2cceeffcbf4f135ab09b6919ec235cef1d
Victoria <vvsecrieru@gmail.com>
2018-12-03 12:59:24 -0500

    test

--- app/src/main/java/com/example/conted/myapplication/MainActivity.java
+++ app/src/main/java/com/example/conted/myapplication/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.conted.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linear);
+    }
+
+
+    public void relative(View view) {
+
     }
 }
